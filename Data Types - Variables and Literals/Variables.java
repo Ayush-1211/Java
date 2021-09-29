48b01c006f0a5466245447decaae156d8109c0e9
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-09-29 19:15:11 +0530

    Data Types - Variables and Literals

---
+++ Data Types - Variables and Literals/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int i = 50;
+        short s = 10;
+        float f = 10.5f;
+        char c = 'A';
+        byte b = 12;
+        
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
