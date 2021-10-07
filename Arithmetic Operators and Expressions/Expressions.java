6fac9a0fc973ef979dbaf2cf4be653f04bd02e29
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-10-07 18:43:59 +0530

    Arithmetic Operators and Expressions

---
+++ Arithmetic Operators and Expressions/Expressions.java
@@ -0,0 +1,27 @@
+package operators.and.expressions;
+
+public class Expressions {
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 15;
+        float c = 12.5f;
+        long z = 123l;
+        int n = 3;
+        double d = 45.5;
+        char g = 40;
+        
+        int x = a + b;
+        
+        float v = c * z;
+        float m = n * c;
+        double o = d * c;
+        
+        int y = g - n;
+        
+        System.out.println(x);
+        System.out.println(v);
+        System.out.println(m);
+        System.out.println(o);
+        System.out.println(y);
+    }
+}
