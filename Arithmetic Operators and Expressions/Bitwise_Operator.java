8962b7cf1f63a78479d4c684831d61c8269a6ebe
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-10-13 18:19:00 +0530

    Arithmetic Operators and Expressions

---
+++ Arithmetic Operators and Expressions/Bitwise_Operator.java
@@ -0,0 +1,15 @@
+package operators.and.expressions;
+
+public class Bitwise_Operator {
+    public static void main(String[] args) {
+        int x=10,y=6,z,a,b;
+        
+        z = x & y;
+        a = x | y;
+        b = x ^ y;
+        
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
