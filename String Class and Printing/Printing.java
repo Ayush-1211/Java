9313891489c0c206265f3502b52eb30b60b1ee22
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-10-29 15:14:02 +0530

    1

--- String Class and Printing/Printing.java
+++ String Class and Printing/Printing.java
@@ -2,6 +2,7 @@ package string.pkgclass.and.printing;
 public class Printing {
     public static void main(String[] args) {
         int x=10,y=20;
+        
         System.out.println("Sum " + x + y);
         System.out.println(x + y + " Sum");
         System.out.println("Sum " + (x + y));
