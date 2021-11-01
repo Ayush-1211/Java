91ad92ff5ec0582f36bacaf547186394f3bdce30
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-01 14:07:49 +0530

    3

--- String Class and Printing/Formatted_Output_2.java
+++ String Class and Printing/Formatted_Output_2.java
@@ -8,7 +8,7 @@ public class Formatted_Output_2 {
         System.out.printf("%5d\n",a);     // Width - %5d means total width is 5
         System.out.printf("%05d\n",a);    // Flag  - %05d means empty space fill with 0
         
-        System.out.printf("%(5d\n",b);    // If number is negetive then it will show value in brackets
+        System.out.printf("%(5d\n",b);    // If number is negative then it will show value in brackets
         System.out.printf("%(5d\n",a);
         
         System.out.printf("%+5d\n",a);  // If number is positive then it will show "+" sign before value otherwise "-"
