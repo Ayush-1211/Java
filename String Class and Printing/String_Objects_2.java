a0e6137146a6f8bba6a75d240f47570db6a0812f
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-01 13:37:54 +0530

    6

--- String Class and Printing/String_Objects_2.java
+++ String Class and Printing/String_Objects_2.java
@@ -4,7 +4,7 @@ public class String_Objects_2 {
         String str1 = "Java";       // It will store in String Pool
         String str2 = "Java";
         String str3 = "java";
-        String str4 = new String("Java");       // It will store in a Heap
+        String str4 = new String("Java");       // It will store in Heap
         
         System.out.println(str1 == str2);
         System.out.println(str1 == str3);
