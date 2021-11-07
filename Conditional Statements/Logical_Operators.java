5b1a8877337de72157f79c38904e7cdbba031ec6
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-07 15:22:05 +0530

    2

---
+++ Conditional Statements/Logical_Operators.java
@@ -0,0 +1,9 @@
+package conditional;
+public class Logical_Operators {
+    public static void main(String[] args) {
+        int a=5,b=10,c=15;
+        
+        System.out.println(a<b && a>c);
+        System.out.println(a<b || a>c);
+    }
+}
