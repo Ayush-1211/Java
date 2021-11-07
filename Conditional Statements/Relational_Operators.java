d970e537d3cbfa14de2b4914c84c09dc32bd25bb
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-07 15:21:33 +0530

    1

---
+++ Conditional Statements/Relational_Operators.java
@@ -0,0 +1,13 @@
+package conditional;
+public class Relational_Operators {
+    public static void main(String[] args) {
+        int a=5, b=10, c=15, d=5;
+        
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+        System.out.println(a==d);
+        System.out.println(a>=d);
+        System.out.println(b<=a);
+    }
+}
