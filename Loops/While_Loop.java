4b2d95e3404f0947931d6293a44c3c10a518ba5a
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-12 10:02:52 +0530

    1

---
+++ Loops/While_Loop.java
@@ -0,0 +1,10 @@
+package loops;
+public class While_Loop {
+    public static void main(String[] args) {
+        int i=1;
+        while (i<100) {            
+            System.out.println(i);
+            i = i * 2;
+        }
+    }
+}
