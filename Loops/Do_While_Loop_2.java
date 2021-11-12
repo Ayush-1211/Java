14fb22096885e17399ec26b44e26aceee79ece13
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-12 10:18:01 +0530

    3

---
+++ Loops/Do_While_Loop_2.java
@@ -0,0 +1,12 @@
+package loops;
+public class Do_While_Loop_2 {
+    public static void main(String[] args) {
+        int i=100;
+        do{
+            System.out.println(i);
+            i=i*2;
+        }while(i<100);
+        
+        // If we use While loop here then it will not print anything in output.
+    }
+}
