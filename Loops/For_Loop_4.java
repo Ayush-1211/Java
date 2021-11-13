1edbf07262772574371b31be9b40b453ea79097a
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-13 09:55:03 +0530

    7

---
+++ Loops/For_Loop_4.java
@@ -0,0 +1,8 @@
+package loops;
+public class For_Loop_4 {
+    public static void main(String[] args) {
+        for(int i=0,j=1;i<=10;i++,j=j*2){
+            System.out.println(i+"-->"+j);
+        }
+    }
+}
