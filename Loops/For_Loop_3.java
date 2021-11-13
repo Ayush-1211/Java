797cae01f81279e00097de0445e172d52c8de3a8
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-13 09:54:44 +0530

    6

---
+++ Loops/For_Loop_3.java
@@ -0,0 +1,10 @@
+package loops;
+public class For_Loop_3 {
+    public static void main(String[] args) {
+        int i=0;
+        for(System.out.println("Hello");i<=10;){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
