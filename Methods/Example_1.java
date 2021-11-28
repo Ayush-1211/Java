2938e4d627e2b2b42c741b0e1d3bb6cc7444cb54
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-11-28 09:41:38 +0530

    8

---
+++ Methods/Example_1.java
@@ -0,0 +1,15 @@
+package methods;
+public class Example_1 {
+    // Prime number or not
+    static boolean isPrime(int n){
+        for(int i=2;i<n/2;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(19));
+    }
+}
