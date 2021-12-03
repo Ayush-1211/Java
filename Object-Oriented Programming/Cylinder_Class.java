932fc2b724e8d165e5e5190ba6a9bbf615106a7c
Ayush Prajapati <63777803+Ayush-1211@users.noreply.github.com>
2021-12-03 09:29:15 +0530

    3

---
+++ Object-Oriented Programming/Cylinder_Class.java
@@ -0,0 +1,31 @@
+package object.oriented.programming;
+
+class Cylinder{
+    public double radius;
+    public double height;
+    public double lidArea(){
+        return Math.PI*radius*radius;
+    }
+    public double totalSurfaceArea(){
+        return 2*lidArea()+circumference()*height;
+    }
+    public double circumference(){
+        return 2*Math.PI*radius;
+    }
+    public double volume(){
+        return lidArea()*height;
+    }
+
+}
+public class Cylinder_Class {
+    public static void main(String[] args) {
+        Cylinder c = new Cylinder();
+        c.radius = 7;
+        c.height = 10;
+        
+        System.out.println(c.lidArea());
+        System.out.println(c.totalSurfaceArea());
+        System.out.println(c.circumference());
+        System.out.println(c.volume());
+    }
+}
